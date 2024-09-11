1adc68644a3fa057a901521a0fa222d190f41db7
vaibhav korat <84782747+Vkorat1012@users.noreply.github.com>
2024-09-11 13:56:11 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Development
+
+
+app = Flask(__name__)
+app.config.from_object(Development)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from views import index
+
+from mod_admin import admin
+from mod_users import users
+from mod_blog import blog
+
+app.register_blueprint(admin)
+app.register_blueprint(users)
+app.register_blueprint(blog)
